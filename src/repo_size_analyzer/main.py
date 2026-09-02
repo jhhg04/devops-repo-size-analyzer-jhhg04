684475b8163573700600244908d08ad3bc0d6a17
jhhg820 <jhhg04@gmail.com>
2026-09-01 19:17:15 -0500

    add module form reports

--- src/repo_size_analyzer/main.py
+++ src/repo_size_analyzer/main.py
@@ -1,5 +1,6 @@
 from config import REPO_PATH, TOP_FILES, TOP_FOLDERS
 from scanner import scan_repository
 from reports import (
-    print_largest_files
+    print_largest_files,
+    print_largest_folders
 )
