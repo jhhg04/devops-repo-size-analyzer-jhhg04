d4794c558070a41001e8f44e0a8065cfce8bebc9
jhhg820 <jhhg04@gmail.com>
2026-09-02 19:38:15 -0500

    add print_summary

--- src/repo_size_analyzer/main.py
+++ src/repo_size_analyzer/main.py
@@ -2,5 +2,6 @@ from config import REPO_PATH, TOP_FILES, TOP_FOLDERS
 from scanner import scan_repository
 from reports import (
     print_largest_files,
-    print_largest_folders
+    print_largest_folders,
+    print_summary,
 )
