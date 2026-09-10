5d35478b249c2e0611b880d6824f920ba3b88ca0
jhhg820 <jhhg04@gmail.com>
2026-09-10 18:16:48 -0500

    add def test_format_size_kilobytes

--- test/test_formatter.py
+++ test/test_formatter.py
@@ -3,3 +3,5 @@ from repo_size_analyzer.formatter import format_size
 def test_format_size_bytes() -> None:
     """Test formatting values smaller than one kilobyte."""
     assert format_size(512) == "512.00 B"
+
+def test_format_size_kilobytes() -> None:    
